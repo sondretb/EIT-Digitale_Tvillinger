b9d65291344a11dd8e78b76b981bf51c4db2a0ef
Sondre Bungum <sondretb@stud.ntnu.no>
2023-03-25 15:17:27 +0100

    feat: added model and dataset class to be filled

---
+++ model.py
@@ -0,0 +1,9 @@
+from torch import nn
+
+class KumlokkModel(nn.Module):
+    def __init(self):
+        super().__init__
+        self.model #Her lages modellen av torch.nn building blocks.
+
+    def forward():
+        pass    
