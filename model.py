a71c089c08d15bc788c3b67c310559bf3f9d1f46
sondretb <63402808+sondretb@users.noreply.github.com>
2023-04-25 11:17:38 +0200

    Delete model.py

--- model.py
+++
@@ -1,9 +0,0 @@
-from torch import nn
-
-class KumlokkModel(nn.Module):
-    def __init(self):
-        super().__init__
-        self.model #Her lages modellen av torch.nn building blocks.
-
-    def forward():
-        pass    
